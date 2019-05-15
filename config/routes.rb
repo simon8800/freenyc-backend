e9a9697fe09a96231ce62966cc53bd7e80ad59d1
Simon Mei <smei8800@gmail.com>
2019-05-15 16:56:40 -0400

    commented out auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories
       resources :courses
+      resources :users, only: [:create, :show]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
     end
   end
 end
