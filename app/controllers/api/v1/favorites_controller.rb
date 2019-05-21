00ae7282ba2b8fb4e6dd3197f0df7970615a8530
Simon Mei <smei8800@gmail.com>
2019-05-21 18:56:29 -0400

    favorites in the making

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,5 +1,14 @@
-class Api::V1::CoursesController < ApplicationController
+class Api::V1::FavoritesController < ApplicationController
   def create
-    
+    @favorite = Favorite.create(favorite_params)
+  end
+
+  def destroy
+    byebug
+  end
+
+  private
+  def favorite_params
+    params.permit(:user_id, :course_id)
   end
 end
